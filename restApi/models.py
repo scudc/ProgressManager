ac18e649d7a29c0d27860fad878e323684c29655
unknown <hobodong@HOBODONG-PC9.tencent.com>
2012-11-12 18:10:16 +0800

    hobo add code

--- restApi/models.py
+++ restApi/models.py
@@ -8,6 +8,9 @@ class ProgressRateData(models.Model):
     progressId = models.IntegerField(help_text='Must be an integer')
     progressName = models.CharField(max_length=32,help_text="progressStat,Max length 32 chars")
     progressStat = models.CharField(max_length=32,help_text="progressStat,Max length 32 chars")
+    #the progress type
+    cmdType = models.CharField(max_length=6,help_text="cmdType,true or false")
+    #
     created = models.DateTimeField(auto_now_add=True)
     remark = models.CharField(max_length=100,help_text="the progress remark")
     
